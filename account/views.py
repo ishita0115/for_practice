8c902cbd5026dc8e6a2d11f91ede010cd6454f52
ishita <ishitachovatiya15@gmail.com>
2024-02-29 17:54:14 +0530

    add a token based authentication2

--- account/views.py
+++ account/views.py
@@ -40,7 +40,6 @@ class UserLoginView(APIView):
              return Response({'errors' : {'non_field_errors':['Email or Password is not Valid']}},status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class UserProfileView(APIView):
     renderer_classes = [UserRenderer]
     permission_classes = [IsAuthenticated]
